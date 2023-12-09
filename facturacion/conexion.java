a083d7da6500cf82cdc8d2bf1bdad442b835adf5
yairhino <145174221+yairhino@users.noreply.github.com>
2023-12-08 21:22:48 -0600

    Add files via upload

---
+++ facturacion/conexion.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturacion;
+import java.sql.*;
+
+/**
+ *
+ * 
+ */
+public class conexion {
+
+   
+   
+   
+   private final String url = "jdbc:mysql://localhost/facturacion";
+    PreparedStatement psPrepararSentencia;
+    Connection con = null;
+   
+
+    
+    public conexion() {
+     try{  
+         
+         Class.forName("com.mysql.jdbc.Driver");
+         
+         con = DriverManager.getConnection(url,"root","");
+         if (con!=null){
+            System.out.println("Conexión a base de datos facturacion. listo");
+         }
+      }
+         catch(SQLException e)
+         {
+         System.out.println(e);
+         }
+         catch(ClassNotFoundException e)
+         {
+          System.out.println(e);
+         }
+    }
+     /**
+     *
+     * @return
+     */
+    public Connection conectado(){
+      return con;
+}
+
+    public void desconectar(){
+      con = null;
+      System.out.println("conexion terminada");
+
+    } 
+
+    
+}
